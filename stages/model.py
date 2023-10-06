09c228272caec1b618ab0499017991aac3aa0c77
Александр Немальцев <aleksandrnemalcev@Aleksandrs-MacBook-Pro.local>
2023-10-06 11:33:21 +0300

    fix score

--- stages/model.py
+++ stages/model.py
@@ -11,6 +11,6 @@ class Recommended:
         res = self.model.predict(image)
         probs_ind = res[0].probs.data.argsort().tolist()[::-1]
         state = res[0].names
-        probs = [round(x, 5) for x in res[0].probs.data[probs_ind].tolist()[::-1]]
+        probs = [round(x, 5) for x in res[0].probs.data[probs_ind].tolist()]
         response = dict(zip([state[i] for i in probs_ind], probs))
         return response
